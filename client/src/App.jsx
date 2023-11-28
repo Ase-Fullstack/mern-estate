ef59968cf35bd8c829629ed8c5aea465c0103348
Ase Shew <asenake.muleapi@gmail.com>
2023-11-28 20:44:27 +0300

    Update the header and make profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Profile from "./pages/Profile";
 import Signin from "./pages/Signin";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 
 export default function App() {
@@ -13,12 +14,14 @@ export default function App() {
       <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/profile' element={<Profile/>}/>
         <Route path='/sign-in' element={<Signin/>}/>
         <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route element={<PrivateRoute/>}>
+          <Route path='/profile' element={<Profile />}/> 
+        </Route>
       </Routes>
   </BrowserRouter>
-  )
+  );
 }
 
