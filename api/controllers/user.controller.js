2df2404eeed0ea750a897603e5000c03cbf78b8e
Ase Shew <asenake.muleapi@gmail.com>
2023-12-03 12:33:24 +0300

    Complete update user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -13,7 +13,7 @@ export const updateUser = async (req, res, next) => {
     if(req.user.id !== req.params.id) return next(errorHandler(401, "You can only update your own account!"));
     try {
         if(req.body.password) {
-            req.body.password = bcryptjs.hashSync(req.body.password, 10)
+            req.body.password = bcryptjs.hashSync(req.body.password, 10);
         }
         const updatedUser = await User.findByIdAndUpdate(req.params.id, {
             $set:{
@@ -21,12 +21,14 @@ export const updateUser = async (req, res, next) => {
                 email: req.body.email,
                 password: req.body.password,
                 avatar: req.body.avatar,
-            }
-        }, {new: true}
+            },
+        }, 
+        {new: true}
         );
         const { password, ...rest } = updatedUser._doc;
+
         res.status(200).json(rest);
     } catch (error) {
-        next(error)
+        next(error);
     }
 };
