cea8ba0d487de9143d4a72cc79c7694266351b5c
Ase Shew <asenake.muleapi@gmail.com>
2023-12-13 22:04:56 +0300

    Complete About page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,7 +27,7 @@ export default function Header() {
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to='/'>
                 <h1 className='fornt-bold text-sm sm:text-xl flex flex-wrap'>
-                    <span className='text-slate-500'>Ase's</span>
+                    <span className='text-slate-500'>Ase</span>
                     <span className='text-slate-700'>Estate</span>
                 </h1>
             </Link>
